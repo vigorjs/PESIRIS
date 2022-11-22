d7b02efb7dc4e7d3abbd471bfe98cb55ae9e8f65
vigorjs <111201912289@mhs.dinus.ac.id>
2022-11-22 16:12:56 +0700

    Update detail.php

--- detail.php
+++ detail.php
@@ -49,7 +49,7 @@ if($gambar==null){
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.php">PESIRIS (Pegadaian Sistem Inventaris)</a>
+            <a class="navbar-brand" href="index.php">PESIRIS</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
         </nav>
         <div id="layoutSidenav">
