2426e684d3a528a08a33832e6495e910256593da
vigorjs <111201912289@mhs.dinus.ac.id>
2022-11-22 16:10:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ require 'cek.php';
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-            <a class="navbar-brand" href="index.php">PESIRIS (Pegadaian Sistem Inventaris)</a>
+            <a class="navbar-brand" href="index.php">PESIRIS</a>
         </nav>
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
