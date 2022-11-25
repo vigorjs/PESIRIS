819a282c4e3600f5d7769dbb4db50e0c3be53e40
vigorjs <111201912289@mhs.dinus.ac.id>
2022-11-25 08:56:36 +0700

    Update keluar.php

--- keluar.php
+++ keluar.php
@@ -159,9 +159,9 @@ require 'cek.php';
                                                 <td><?=$penerima;?></td>
                                                 <td><?=$qty;?></td>
                                                 <td>
-                                                    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit<?=$idb;?>"> <i class="fa fa-edit"> </i></#>
+                                                    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit<?=$idk;?>"> <i class="fa fa-edit"> </i></#>
                                                     </button>
-                                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete<?=$idb;?>"> <i class="fa fa-trash"> </i></#>
+                                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete<?=$idk;?>"> <i class="fa fa-trash"> </i></#>
                                                     </button>
                                                 </td>
                                                 
