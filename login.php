86629fd8b5c97cd2df656c6fc36f208a06f65ee2
Virgo Fajar Pamungkas <111201912289@mhs.dinus.ac.id>
2023-07-05 01:54:58 +0700

    add register

---
+++ login.php
@@ -0,0 +1,94 @@
+<?php
+require 'function.php';
+
+// cek login
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+// cocokin dengan db, mencari datanya ada atau ga
+    $cekdatabase = mysqli_query($conn, "SELECT * FROM login where email='$email' and password='$password'");
+//hitung jumlah data
+    $hitung = mysqli_num_rows($cekdatabase);
+
+    if($hitung>0){
+
+        $_SESSION['log'] = 'True';
+        header('location:index.php');
+    } else{
+        header('location:login.php');
+    }
+};
+
+if(!isset($_SESSION['log'])){
+
+} else{
+    header('location:index.php');
+}
+?>
+<!doctype html>
+<html lang="en" class="fullscreen-bg">
+
+<head>
+<title>LOGIN PT. PEGADAIAN</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
+	<link rel="stylesheet" href="assets/css/main.css">
+	<link rel="stylesheet" href="assets/css/demo.css">
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/logo2.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/logo2.png">
+</head>
+
+<body background='assets/img/Screenshot (133).png'>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+		<div class="vertical-align-wrap">
+			<div class="vertical-align-middle">
+				<div class="auth-box ">
+					<div class="left">
+						<div class="content">
+							<div class="header">
+								<p class="lead">LOGIN</p>
+							</div>
+							<form action="login.php" method="post" enctype="multipart/form-data">
+								<div class="form-group">
+									<label for="signin-email" class="control-label sr-only">Email</label>
+									<input type="text" name="email" class="form-control" placeholder="email" required>
+								</div>
+								<div class="form-group">
+									<label for="signin-password" class="control-label sr-only">Password</label>
+									<input type="password" name="password" class="form-control" placeholder="password" required>
+								</div>
+								<p>Belum punya akun? <a href="register.php">Daftar</a></p>
+								<input type="submit" name="login" class="btn btn-primary btn-lg btn-block" value="LOGIN">
+							</form>
+							<br>
+							<center>
+								<p>Created By Magenta </p>
+							</center>
+						</div>
+					</div>
+				  <div class="right">
+						<div class="overlay"></div>
+						<div class="content text text-center">
+						  <img src="assets/img/logo2.png" alt="logo-icon" width="268" height="136">
+						  <img src="assets/img/logoa.png" alt="logo-icon" width="140px" height="140px">
+						  <p>Scan QR untuk cek persediaan barang</p>
+							<br>
+							
+							<h1 class="heading">PEGADAIAN SISTEM INVENTARIS</h1>
+
+						</div>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
+</body>
+
+</html>
---
+++ stockbarang.sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 04, 2023 at 08:51 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stockbarang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keluar`
+--
+
+CREATE TABLE `keluar` (
+  `idkeluar` int(11) NOT NULL,
+  `idbarang` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `penerima` varchar(25) NOT NULL,
+  `penyerah` varchar(25) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `keluar`
+--
+
+INSERT INTO `keluar` (`idkeluar`, `idbarang`, `tanggal`, `penerima`, `penyerah`, `qty`) VALUES
+(23, 67, '2023-05-15 21:39:15', 'Kantor Cabang Kebumen', 'Bu Ning', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `iduser` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`iduser`, `email`, `password`) VALUES
+(1, 'virgofajar123@gmail.com', '123'),
+(6, 'ifantowus@gmail.com', '123123'),
+(7, 'mayang', '123'),
+(8, 'pundy', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `masuk`
+--
+
+CREATE TABLE `masuk` (
+  `idmasuk` int(11) NOT NULL,
+  `idbarang` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `keterangan` varchar(25) NOT NULL,
+  `penyerah` varchar(25) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `masuk`
+--
+
+INSERT INTO `masuk` (`idmasuk`, `idbarang`, `tanggal`, `keterangan`, `penyerah`, `qty`) VALUES
+(31, 67, '2023-05-15 21:38:01', 'Towud', 'Bu Ning', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `idbarang` int(11) NOT NULL,
+  `namabarang` varchar(25) NOT NULL,
+  `deskripsi` varchar(25) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `image` varchar(99) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stock`
+--
+
+INSERT INTO `stock` (`idbarang`, `namabarang`, `deskripsi`, `stock`, `image`) VALUES
+(65, 'Brosur KUR', 'Marketing Kit', 100, 'b18affd703221cf977be17c0ceb9c55b.png'),
+(67, 'Payung', 'Marketing KIT', 100, '48f288f7c4c909aa339baf860128b88e.jpg'),
+(68, 'Kaos', 'Marketing KIT', 60, 'c97ad007ef458dd3fcae22a5ecc0fa92.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `keluar`
+--
+ALTER TABLE `keluar`
+  ADD PRIMARY KEY (`idkeluar`),
+  ADD KEY `idbarang` (`idbarang`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`iduser`);
+
+--
+-- Indexes for table `masuk`
+--
+ALTER TABLE `masuk`
+  ADD PRIMARY KEY (`idmasuk`),
+  ADD KEY `idbarang` (`idbarang`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`idbarang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `keluar`
+--
+ALTER TABLE `keluar`
+  MODIFY `idkeluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `masuk`
+--
+ALTER TABLE `masuk`
+  MODIFY `idmasuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `idbarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `keluar`
+--
+ALTER TABLE `keluar`
+  ADD CONSTRAINT `keluar_ibfk_1` FOREIGN KEY (`idbarang`) REFERENCES `stock` (`idbarang`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `masuk`
+--
+ALTER TABLE `masuk`
+  ADD CONSTRAINT `masuk_ibfk_1` FOREIGN KEY (`idbarang`) REFERENCES `stock` (`idbarang`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
